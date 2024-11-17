0f855a110ab4547b94cd1c292946b5a661137702
Harishwarreddy3739 <harishwarreddy3739@gmail.com>
2024-11-17 21:26:27 +0000

    Add files via upload
    first

---
+++ myapp/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        kotlin("jvm") version "2.0.21"
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "myapp"
+include(":app")
+ 
